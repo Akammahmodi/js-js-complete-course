84bffe222cff82ca571741dd9030ca173101b868
akammahmodi <akammahmodi88@gmail.com>
2021-04-22 14:39:56 +0200

    changes

--- 01-base/04-agt-confirm/script.js
+++ 01-base/04-agt-confirm/script.js
@@ -10,7 +10,21 @@
 // You will have time to focus on it later.
 
 (function() {
+    let age = prompt('How old are you?',);
+    alert(`You are ${age} years old!`);
+    let gender = prompt('What is your gender',);
+    alert(`You are ${gender}`);
+    let city = prompt('Where do you live?',);
+    alert(`You live in ${city}`);
 
-    // your code here
 
-})();
+    var resault = confirm ("You filled in: " + age + " years old, " + gender + ", from " + city + ". Is this correct?");
+
+    if (resault === true) {
+        alert("It confirmed");
+    } else if (resault === false) {
+        alert("Please give correct information");
+location.reload();
+    }
+}
+())"You filled in: " + age + " years old, " + gender + ", from " + town + ". Is this correct?"
